76dbcc7e0fb8a29c5d8bdbe0f83c022142eb8c25
ymasaki-dot <y.masaki0526@gmail.com>
2021-03-05 01:08:45 +0000

    topic1

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '64056f86d5fd88f4365040a5a05a47282fc961468b37d83704e498b3abc747bd6c40f6f25ea6276fafd952bd488e4b5391b1737a5c9d3104bf58762371091eb1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :books, only:[:new, :create, :index, :show, :destroy]
+  resources :users, only:[:show, :edit, :update]
 end
