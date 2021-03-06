ea4770c30fdc4be80c7259c061e5d53647d56cb5
ymasaki-dot <y.masaki0526@gmail.com>
2021-03-06 09:15:42 +0000

    [finish]ステップ２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,42 @@
 class BooksController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def index
     @book = Book.new
     @books = Book.all
     @book.user_id = current_user.id
-    
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+   if @book.save
+     redirect_to book_path(@book.id)
+   else
+     render:index
+   end
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
-  
+
   def edit
     @book = Book.find(params[:id])
+    if @book.user_id = current_user.id
+      render:edit
+    else
+      redirect_to book_path
+    end
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
    book = Book.find(params[:id])
    book.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
@@ -8,22 +9,30 @@ class UsersController < ApplicationController
 
    def index
     @users = User.all
-    @user = current_user.id
+    @user = current_user
    end
 
   def edit
     @user = User.find(params[:id])
+    if @user.id = current_user.id
+        render:edit
+    else
+        redirect_to user_path(@user.id)
+    end
   end
 
  def update
-   user = User.find(params[:id])
-   user.update(user_params)
-   redirect_to user_path(user.id)
+   @user = User.find(params[:id])
+   if @user.update(user_params)
+    redirect_to user_path(@user.id)
+   else
+       render:edit
+   end
  end
 
  private
  def user_params
    params.require(:user).permit(:name, :introduction, :profile_image)
  end
- 
+
 end
