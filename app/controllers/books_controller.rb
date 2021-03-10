aed4ef3445d3dc696bc0f37128dc4342077d6333
ymasaki-dot <y.masaki0526@gmail.com>
2021-03-10 11:15:37 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,18 +26,21 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    if @book.user_id = current_user.id
+    if @book.user == current_user
       render :edit
     else
-      redirect_to book_path
+      redirect_to books_path
     end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "You have update book successfully."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+        flash[:notice] = "You have update book successfully."
+        redirect_to book_path(@book.id)
+    else
+       render :edit
+    end
   end
 
   def destroy
